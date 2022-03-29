3253063799a1e61ed61b27c7e2f4f14f4eaf9241
Brad Buran <buran@ohsu.edu>
2022-03-29 07:08:19 -0700

    Ensure we can stop websocket server when video.stop is set

--- psivideo/tcp.py
+++ psivideo/tcp.py
@@ -5,6 +5,7 @@ import asyncio
 from functools import partial
 import json
 
+from threading import Thread
 import websockets
 
 
@@ -12,16 +13,31 @@ async def handle_connection(video, websocket):
     async for mesg in websocket:
         try:
             result = video.dispatch(**json.loads(mesg))
-            print(mesg, result)
             await websocket.send(json.dumps(result))
+        except websockets.exceptions.ConnectionClosedOK:
+            pass
         except Exception as e:
             await websocket.send(json.dumps(str(e)))
 
 
+async def run_server(video, loop):
+    cb = partial(handle_connection, video)
+    async with websockets.serve(cb, video.hostname, video.port, loop=loop):
+        while True:
+            await asyncio.sleep(0.1)
+            if video.stop.is_set():
+                break
+
+
+def start_server(video, loop):
+    loop.run_until_complete(run_server(video, loop))
+
+
 def video_tcp(video):
     loop = asyncio.new_event_loop()
-    cb = partial(handle_connection, video)
-    server = websockets.serve(cb, video.hostname, video.port, loop=loop)
-    loop.run_until_complete(server)
+    cb = partial(start_server, video, loop)
+    thread = Thread(target=cb)
+    thread.start()
     print(f'Websocket server listening on ws://{video.hostname}:{video.port}')
-    loop.run_forever()
+    thread.join()
+    print('Websocket server shutting down')
